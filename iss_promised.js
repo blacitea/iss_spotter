d164896a0d78c425ffdca40f749271dfaf4ca83b
Angel Tsang <blacitea@gmail.com>
2020-07-31 19:21:56 +0000

    chain with .then

--- iss_promised.js
+++ iss_promised.js
@@ -1,1 +1,20 @@
 const request = require("request-promise-native");
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+
+
+const fetchMyIP = () => {
+  // the request here return a promise at run, we want the outer function to return the promise from inner to outside
+  return request("https://api.ipify.org?format=json");
+};
+
+const fetchCoordsByIP = function (body) {
+  let ip = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/${ip}`);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
